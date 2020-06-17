5d5594991497d34feeae448211c59980fe44d6f6
Daniel Amaral <silva.daniel86@gmail.com>
2020-06-17 13:29:49 -0300

    Primeira versão do primeiro loop do Código do Artigo + Adição do GITIGNORE

--- main.R
+++ main.R
@@ -4,31 +4,49 @@
 #    using a simulation approach
 ########################################################
 
+## libraries
+require(Rfast)
+
 ## hyperparameters
-N = 4 # 100 rows in dataset
-n = 4 # 100 iterations of simulation
+N <- 100 # 100 rows in dataset
+n <- 100 # 100 iterations of simulation
+alpha <- 0.05
 
 ## matrix's used
 
 # S is a [N x n] matrix which contains in the rows the 
 # order statistics of studentized cond. residuals (CR)
-S = matrix(runif(N*n, 1, 10), nrow = N) # pop. random
+S <- matrix(runif(N*n, 1, 10), nrow = N) # pop. random
 
 # D is a [N x n] matrix which contains the absolute 
 # standardized values of matrix S
-D = abs(scale(S))
+D <- abs(scale(S))
 
 # Matrix C contains rank-values computed within each 
 # column of matrix S.
-C = colRanks(S)
+C <- colRanks(S)
 
 ## vectors used
 
 # c is a [N x 1] vector which contains the most extreme 
 # rank value of the rows of C.
-c = apply(C, 1, max)
+c <- apply(C, 1, max)
 
 ## main loop
-while(nrow(S) - lenght(theta) >= (1-alpha)*N) {
+k <- 1
+repeat {
+  theta_k <- which(c == min(c))
+  S <- S[-theta_k,]
+  c <- c[-theta_k]
   
+  theta_k_new <- which(c == min(c))
+  if(nrow(S) - length(theta_k_new) < (1-alpha)*N) {
+    break
+  }
+  k <- k + 1
 }
+
+
+
+
+
