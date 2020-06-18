4f9c4ed0296341cf4d322d0804b547b246396759
Daniel Amaral <silva.daniel86@gmail.com>
2020-06-17 23:15:43 -0300

    main loop atualizado... pequeno bug

--- main.R
+++ main.R
@@ -8,15 +8,16 @@
 require(Rfast)
 
 ## hyperparameters
-N <- 100 # 100 rows in dataset
-n <- 100 # 100 iterations of simulation
-alpha <- 0.05
+n <- 30 # 50 rows in dataset
+N <- 1000 # 1000 iterations of simulation
+alpha <- 0.05 # significance level
 
 ## matrix's used
 
 # S is a [N x n] matrix which contains in the rows the 
 # order statistics of studentized cond. residuals (CR)
 S <- matrix(runif(N*n, 1, 10), nrow = N) # pop. random
+S <- t(apply(S, 1, sort)) # order statitics
 
 # D is a [N x n] matrix which contains the absolute 
 # standardized values of matrix S
@@ -31,11 +32,14 @@ C <- colRanks(S)
 # c is a [N x 1] vector which contains the most extreme 
 # rank  value of the rows of C.
 c <- apply(C, 1, max)
+c <- N - c + 1
 
 ## main loop
 k <- 1
 repeat {
+  # 1.
   theta_k <- which(c == min(c))
+  # 2.
   S <- S[-theta_k,]
   c <- c[-theta_k]
   
@@ -45,8 +49,9 @@ repeat {
   }
   k <- k + 1
 }
-D_theta <- D[-theta_k,]
-C_theta <- C[-theta_k,]
+# 3.
+D_theta <- D[theta_k,]
+C_theta <- C[theta_k,]
 
 
 
